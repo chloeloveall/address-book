ac0bd514871ced3bd1ecc73e39e0995f8de1abdc
Jon Stump <jmstump@gmail.com>
2021-01-25 19:27:50 -0800

    update scripts.js to make sure some variables weren't referenced with a . and #

--- js/scripts.js
+++ js/scripts.js
@@ -56,12 +56,12 @@ function displayContactDetails(addressBookToDisplay) {
 function showContact(contactId) {
   const contact = addressBook.findContact(contactId);
   $("#show-contact").show();
-  $("#.first-name").html(contact.firstName);
-  $("#.last-name").html(contact.lastName);
-  $("#.phone-number").html(contact.phoneNumber);
-  let buttons = $("$buttons");
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  let buttons = $("#buttons");
   buttons.empty();
-  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
+  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
 }
 
 function attachContactListeners() {
@@ -81,6 +81,11 @@ $(document).ready(function() {
     const inputtedFirstName = $("input#new-first-name").val();
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+
     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
